7781d885c1b0139a6db0529a6fae22c2807f9e6f
Agnieszka Lis <agnieszkalis@gmail.com>
2017-09-05 21:48:57 +0100

    Added balance with 0 to start with

--- spec/mockup_oystercard_spec.rb
+++ spec/mockup_oystercard_spec.rb
@@ -1,3 +1,8 @@
 require 'mockup_oystercard'
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
+
+  it 'should have a balance of 0 to start with' do
+    expect(oystercard.balance).to eq(0)
+  end
 end
